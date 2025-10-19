b740c85f57b6d8e186923524383c86f0db647f37
NopeNope <kingdutchisbad+12@proton.me>
2025-10-19 23:39:59 +0530

    Change NINJA_REMOTE_NUM_JOBS to 64
    Reduce the number of remote jobs for Ninja from 350 to 64.

--- gsi.sh
+++ gsi.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+/opt/crave/resync.sh
 source build/envsetup.sh
 git clone https://github.com/NopeNopeGuy/reclient --depth=1
 export USE_RBE=1
 export RBE_DIR=$(pwd)/reclient
-export NINJA_REMOTE_NUM_JOBS=350
+export NINJA_REMOTE_NUM_JOBS=64
 export RBE_R8_EXEC_STRATEGY=local
 export RBE_D8_EXEC_STRATEGY=local
 export RBE_JAVAC_EXEC_STRATEGY=local
